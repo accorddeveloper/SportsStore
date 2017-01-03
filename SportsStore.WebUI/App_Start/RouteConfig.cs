63b9518f19f479292c93762bd8ed4b0b540bf012
Michael Tran <michael_tran92@hotmail.com>
2017-01-03 19:34:20 +1100

    Add navigation buttons

--- SportsStore.WebUI/App_Start/RouteConfig.cs
+++ SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -13,17 +13,37 @@ namespace SportsStore.WebUI
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // URL: /
+            routes.MapRoute(null,
+                            "",
+                            new
+                            {
+                                controller = "Product", action = "List",
+                                category = (string)null, page = 1
+                            });
+            
+            // URL: /Page2
             routes.MapRoute(
-                name: null,
-                url: "Page{page}",
-                defaults: new {Controller = "Product", action = "List"}
+                            null,
+                            "Page{page}",
+                            new {controller = "Product", action = "List", category = (string)null },
+                            new { page = @"\d+" }
             );
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Product", action = "List", id = UrlParameter.Optional }
+            // URL: /Soccer
+            routes.MapRoute(null,
+                "{category}",
+                new { controller = "Product", action = "List", page = 1 }
             );
+
+            // URL: /Soccer/Page2
+            routes.MapRoute(null,
+                            "{category}/Page{page}",
+                            new {controller = "Product", action = "List"},
+                            new {page = @"\d+"}
+            );
+
+            routes.MapRoute(null, "{controller}/{action}");
         }
     }
 }

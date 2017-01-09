8c346cebadfb134f6d08932335b95cc59443ac9b
Michael Tran <michael_tran92@hotmail.com>
2017-01-09 23:16:12 +1100

    Add Edit view

--- SportsStore.WebUI/Controllers/AdminController.cs
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -1,5 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Controllers
 {
@@ -15,6 +17,13 @@ namespace SportsStore.WebUI.Controllers
         public ViewResult Index()
         {
             return View(repository.Products);
+        }
+
+        public ViewResult Edit(int productId)
+        {
+            Product product = repository.Products
+                                        .FirstOrDefault(p => p.ProductID == productId);
+            return View(product);
         }
     }
 }
